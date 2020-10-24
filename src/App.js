ee255b4cea267712314a3d97d5926952e7e20ae0
Denzel <denzeltlee@gmail.com>
2020-10-24 21:52:17 +0800

    Setup initial states

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from 'react';
+import useInterval from './useInterval';
+import { CANVAS_SIZE, SNAKE_START, APPLE_START, SCALE, SPEED, DIRECTIONS } from './constants';
 
-const App = () => <div>Snake Game</div>
-  
+const App = () => {
+    const canvasRef = useRef();
+    const [snake, setSnake] = useState(SNAKE_START);
+    const [apple, setApple] = useState(APPLE_START);
+    const [dir, setDir] = useState([0, -1]);
+    const [speed, setSpeed] = useState(null);
+    const [gameover, setGameover] = useState(false);
+
+    const startGame = () => {};
+
+    const endGame = () => {};
+
+    const moveSnake = () => {};
+
+    return (
+        <div role="button" tabIndex="0" onKeyDown={(e) => moveSnake(e)}>
+            <canvas style={{ border: '1px solid black' }} ref={canvasRef} width={`${CANVAS_SIZE[0]}px`} height={`${CANVAS_SIZE[1]}px`} />
+            {gameover && <div>GAME OVER!</div>}
+            <button onClick={startGame}>Start Game</button>
+        </div>
+    );
+};
+
 export default App;
