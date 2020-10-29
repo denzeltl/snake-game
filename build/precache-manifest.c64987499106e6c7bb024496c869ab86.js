156c6c79029b1040b0ef603ee44d632657c84873
Denzel <denzeltlee@gmail.com>
2020-10-29 20:45:18 +0800

    Deploy with gh-pages

---
+++ build/precache-manifest.c64987499106e6c7bb024496c869ab86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3886faff63aeb65a6c41ad81a6119bb2",
+    "url": "/snake-game/index.html"
+  },
+  {
+    "revision": "065c699c4d8a5d9fbe0a",
+    "url": "/snake-game/static/js/2.491d0b30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snake-game/static/js/2.491d0b30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d6d214fa20523fb3e3b",
+    "url": "/snake-game/static/js/main.ebd71f22.chunk.js"
+  },
+  {
+    "revision": "07ffda929275a2a1950d",
+    "url": "/snake-game/static/js/runtime-main.aba17d45.js"
+  }
+]);
